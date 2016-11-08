510d4dd223d99347872cc7aed16468e58146f9ec
Sean <xiamiliu@gmail.com>
2016-11-07 22:33:44 -0600

    mergeSortList

--- src/com/ccsi/sorts/MergeSortList.java
+++ src/com/ccsi/sorts/MergeSortList.java
@@ -1,55 +1,85 @@
 package com.ccsi.sorts;
 
 import java.util.List;
+import java.util.prefs.NodeChangeEvent;
 
 /**
  * Created by gxliu on 2016/11/6.
  */
 public class MergeSortList {
-    public static void main(String[] args) {
-
-    }
-
-    class ListNode{
-        public int val;
-        ListNode next;
-
-        public ListNode(int val) {
-            this.val = val;
-        }
-    }
     private static ListNode head=null;
     private static ListNode tail=null;
-    private static int size=0;
 
-    public MergeSortList(int[] a) {
-        if(a==null||a.length==0)return;
-        for(int i:a){
-            if(size==0){
-                head=new ListNode(i);
-                tail=head;
-            }else {
-                tail.next=new ListNode(i);
-                tail=tail.next;
+    public boolean isEmpty(){
+        return head==null;
+    }
+
+    public ListNode add(int val){
+        if(head==null){
+            head=new ListNode(val);
+            tail=head;
+        }else{
+            tail.next=new ListNode(val);
+            tail=tail.next;
+        }
+        return head;
+    }
+
+    public void display(){
+        System.out.println("List items from first to last:");
+        ListNode curr=head;
+        while(curr!=null){
+            curr.displayNode();
+            curr=curr.next;
+        }
+    }
+
+    public ListNode getFirst(){
+        return head;
+    }
+
+    public ListNode mergeSort(ListNode originalHead) {
+        if(originalHead==null||originalHead.next==null)return originalHead;
+
+        ListNode a=originalHead;
+        ListNode b=originalHead.next;  //由于后面截断a用的是next，须从next开始，否则前后两半分布不均匀
+
+        while(b!=null&&b.next!=null){   //找中点，分成a，b两lists；
+            originalHead=originalHead.next;
+            b=b.next.next;
+        }
+        b=originalHead.next;
+        originalHead.next=null;
+        return merge(mergeSort(a),mergeSort(b));
+    }
+
+    public ListNode merge(ListNode a,ListNode b){
+        ListNode dummyHead=new ListNode(Integer.MAX_VALUE);
+        ListNode c=dummyHead;
+        while(a!=null&&b!=null){
+            if(a.val<b.val){
+                c.next=a;
+                a=a.next;
+            }else{
+                c.next=b;
+                b=b.next;
             }
-            size++;
+            c=c.next;
         }
+        c.next=(a==null)?b:a;
+        head=dummyHead.next;
+        return dummyHead.next;
     }
+}
+class ListNode{
+    public int val;
+    ListNode next;
 
-    public static void merge(ListNode head){
-        if(size==0||size==1)return;
-        ListNode mi=findMid(head);
-
+    public ListNode(int val) {
+        this.val = val;
     }
-    public static ListNode findMid(ListNode head){
-        if(size==0||size==1)return head;
-        ListNode slow=head;
-        ListNode fast=head;
-        if(fast!=null&&fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-        }
-        return slow;
+    public void displayNode(){
+        System.out.println("["+this.val+"]");
     }
 }
 
