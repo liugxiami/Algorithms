ebd06a5fa5ddc83b0224ca0a8571d68f79083536
Sean <xiamiliu@gmail.com>
2016-12-16 23:11:55 -0600

    SegmentTree

--- src/com/ccsi/tree/SegmentTree.java
+++ src/com/ccsi/tree/SegmentTree.java
@@ -34,23 +34,26 @@ public class SegmentTree {
         numbers=nums;
         root=buildSegmentTree(nums,0,nums.length-1);
     }
+    //与构建BSTtree类似，但有一步不同，就是更新特征值
     private Node buildSegmentTree(int[] nums,int start,int end){
-        if(start==end)return new Node(nums[start],start,end);
+        if(start==end)return new Node(nums[start],start,end);   //start==end 就是叶子节点，特征值就是相应数组里的值
         int mi=start+(end-start)/2;
         Node root=new Node(start,end);
         root.left=buildSegmentTree(nums,start,mi);
-        root.right=buildSegmentTree(nums,mi+1,end);
+        root.right=buildSegmentTree(nums,mi+1,end);          //left 开始到mi和right则重mi+1开始到end，该性质在query时也要用到
         root.min=Math.min(root.left.min,root.right.min);    //更新特征值
         return root;
     }
+
     public void update(int i,int val){
         insert(i,val,root);
         numbers[i]=val;
     }
+
     private void insert(int i,int val,Node curr){
         if(curr.start==curr.end){
             curr.min=val;
-            return;
+            return;    //这个return不能省，为什么？
         }
 
         int mi=curr.start+(curr.end-curr.start)/2;
