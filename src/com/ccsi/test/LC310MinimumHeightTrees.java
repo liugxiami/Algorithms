e74961638b48ea1985342ffe019b4e3b6fc8e6d6
Sean <xiamiliu@gmail.com>
2016-12-29 21:31:58 -0600

    LC30BFS

--- src/com/ccsi/test/LC310MinimumHeightTrees.java
+++ src/com/ccsi/test/LC310MinimumHeightTrees.java
@@ -38,8 +38,11 @@ public class LC310MinimumHeightTrees {
             nodes[edges[i][1]].children.add(nodes[edges[i][0]]);
         }
 
-        List<Integer> path1=findLongestPath(nodes[0],nodes);
-        List<Integer> path2=findLongestPath(nodes[path1.get(path1.size()-1)],nodes);
+        //List<Integer> path1=findLongestPath(nodes[0],nodes);
+        //List<Integer> path2=findLongestPath(nodes[path1.get(path1.size()-1)],nodes);
+
+        List<Integer> path1=findLongestPathBFS(nodes[0],nodes);
+        List<Integer> path2=findLongestPathBFS(nodes[path1.get(path1.size()-1)],nodes);
 
         int size=path2.size();
         if(size%2==0){
@@ -61,7 +64,7 @@ public class LC310MinimumHeightTrees {
         path.push(root.val);                          //用stack比用list方便
         visited.add(root.val);                        //标记是否被访问过
         for(Node child:root.children){
-            if(!visited.contains(child)){
+            if(!visited.contains(child.val)){
                 dfs(child,nodes,path,max,visited);    //BT总是在判断语句之后进入下一层，保证传入不为空。
             }
         }
@@ -75,6 +78,45 @@ public class LC310MinimumHeightTrees {
         visited.remove(root.val);
     }
 
+    private List<Integer> findLongestPathBFS(Node root,Node[] nodes){
+        List<Integer> res=new ArrayList<>();
+
+        Queue<List<Node>> queue=new LinkedList<>();   //queue里面存的是node的list，不是平常的node；所以需要多些处理
+        List<Node> init=new ArrayList<>();
+        init.add(root);
+        queue.offer(init);
+
+        Set<Integer> visited=new HashSet<>();
+
+        List<Node> result=new ArrayList<>();
+        while(!queue.isEmpty()){
+            Queue<List<Node>> tempQueue=new LinkedList<>(); //BFS中按层记录
+            while(!queue.isEmpty()){
+                List<Node> curr=queue.poll();
+                Node currNode=curr.get(curr.size()-1);      //list中最后面的node
+
+                result=curr;                                //更新最长路径上的nodes
+                visited.add(currNode.val);
+
+                for(Node n:currNode.children){
+                    if(!visited.contains(n.val)){
+                        List<Node> nextList=new ArrayList<>(curr);
+                        nextList.add(n);
+                        tempQueue.add(nextList);
+                    }
+                }
+            }
+            queue=tempQueue;
+        }
+
+
+        for (int i = 0; i < result.size(); i++) {
+           res.add(result.get(i).val);
+        }
+
+        return res;
+    }
+
     private Node createIfNotExist(Integer index,Node[] nodes ){
         if(nodes[index]==null){
             nodes[index]=new Node(index);
@@ -82,5 +124,13 @@ public class LC310MinimumHeightTrees {
         return nodes[index];
     }
 
+    public static void main(String[] args) {
+        int[][] edges={{0, 3}, {1, 3}, {2, 3}, {4, 3}, {5, 4}};
+        LC310MinimumHeightTrees MHT=new LC310MinimumHeightTrees();
+        List<Integer> result=MHT.findMinHeightTrees(6,edges);
+        for (int i = 0; i < result.size(); i++) {
+            System.out.println(result.get(i));
+        }
+    }
 
 }
