499fcd21f1087f889f6d9f6c0f81e22946dfad66
Sean <xiamiliu@gmail.com>
2017-01-02 23:47:19 -0600

    LC452MinNumArrows

--- src/com/ccsi/test/LC452findMinArrow.java
+++ src/com/ccsi/test/LC452findMinArrow.java
@@ -16,16 +16,16 @@ public class LC452findMinArrow {
                 if(pointA[0]==pointB[0])return pointA[1]-pointB[1];
                 else return pointA[0]-pointB[0];
             }
-        });
+        });    //对二维数组排序，按第一个数字排，如果相等则按第二位排
 
-        int minimunArrows=1;
-        int arrowLimit=points[0][1];
+        int minimunArrows=1;    //最少一枚箭
+        int arrowLimit=points[0][1];   //第一组的末端
         for (int i = 1; i < points.length; i++) {
             if(points[i][0]<arrowLimit){
-                arrowLimit=Math.min(arrowLimit,points[i][1]);
+                arrowLimit=Math.min(arrowLimit,points[i][1]);   //贪心算法保留最小的末端
             }else{
                 minimunArrows++;
-                arrowLimit=points[i][1];
+                arrowLimit=points[i][1];                         //新建一只箭
             }
         }
         return minimunArrows;
