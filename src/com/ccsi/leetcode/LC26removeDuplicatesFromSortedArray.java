29384b29ac153a934514cbcca75a460f003ccfa2
Sean <xiamiliu@gmail.com>
2017-03-07 23:42:28 -0600

    LC85

--- src/com/ccsi/leetcode/LC26removeDuplicatesFromSortedArray.java
+++ src/com/ccsi/leetcode/LC26removeDuplicatesFromSortedArray.java
@@ -5,15 +5,18 @@ package com.ccsi.leetcode;
  */
 public class LC26removeDuplicatesFromSortedArray {
     public static void main(String[] args) {
-        int[] arr={1,2,2,3,4,5,5,6,7,7};
+        int[] arr={1,1,2,2,2,2,3,4,5,5,6,7,7};
         System.out.println(removeDuplicates(arr));
     }
+    //相当于是快慢指针，慢指针用来记录结果数组，覆盖原先的数组，快指针在前面跑，并检查
+    //当前是否与其前一个数相同（用新索引来更容易理解），相同则跳过，不同则交给慢指针。
     public static int removeDuplicates(int[] nums){
         if(nums==null||nums.length==0)return -1;
-        int idx=1;
+        int idx=0;
         for (int i=1; i < nums.length; i++) {
-            if(nums[i]!=nums[i-1])nums[idx++]=nums[i];
+            if(nums[i]==nums[idx]) continue;
+            nums[++idx]=nums[i];
         }
-        return idx;
+        return idx+1;
     }
 }
